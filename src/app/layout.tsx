4b2d17ad149da91e46999aee6940a9780c2ec83b
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2024-10-09 12:10:08 -0300

    Add inter variable font and global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,11 @@ import { Inter } from 'next/font/google';
 
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
   title: `TypeScript starter for Next.js by João Pedro Schmitz`,
@@ -13,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>{children}</body>
     </html>
   );
 }
