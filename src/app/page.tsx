3b0b77d2efdcbd6f31bdf48e6a920bf7489c5222
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2024-10-10 08:22:06 -0300

    Initial structure of the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,56 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
+import { LightBulbIcon } from '@heroicons/react/16/solid';
+import { HomeIcon, QuestionMarkCircleIcon, SparklesIcon } from '@heroicons/react/20/solid';
+
+import { Navbar, NavbarItem, NavbarSection, NavbarSpacer } from '@/lib/ui/navbar';
+import { Sidebar, SidebarBody, SidebarHeader, SidebarItem, SidebarLabel, SidebarSection } from '@/lib/ui/sidebar';
+import { SidebarLayout } from '@/lib/ui/sidebar-layout';
+
+export default function Homepage() {
+  return (
+    <SidebarLayout
+      navbar={
+        <Navbar>
+          <NavbarSpacer />
+          <NavbarSection>
+            <NavbarItem href="/inbox" aria-label="Inbox">
+              Logo
+              {/*<Avatar src="/profile-photo.jpg" square />*/}
+            </NavbarItem>
+          </NavbarSection>
+        </Navbar>
+      }
+      sidebar={
+        <Sidebar>
+          <SidebarHeader>
+            <SidebarLabel>Invoicy</SidebarLabel>
+          </SidebarHeader>
+          <SidebarBody>
+            <SidebarSection>
+              <SidebarItem href="/">
+                <HomeIcon />
+                <SidebarLabel>Home</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="mailto:hey@joaopedro.dev">
+                <QuestionMarkCircleIcon />
+                <SidebarLabel>Support</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/changelog">
+                <SparklesIcon />
+                <SidebarLabel>Changelog</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/share-feedback">
+                <LightBulbIcon />
+                <SidebarLabel>Share feedback</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+            {/*<SidebarSection className="max-lg:hidden">*/}
+            {/*  <SidebarHeading>Advertisement</SidebarHeading>*/}
+            {/*</SidebarSection>*/}
+          </SidebarBody>
+        </Sidebar>
+      }
+    >
+      Hello World!
+    </SidebarLayout>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-sans)'],
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
       },
     },
   },
