71f92b07783e2b8c6832ac5488e36e5fb808dc9d
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2026-04-20 12:12:21 -0300

    Remove dummy text

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -48,9 +48,6 @@ export default function App() {
       </div>
       <div className="mx-auto max-w-2xl text-center">
         <h1 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Create Invoice</h1>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
       </div>
       <div className="mx-auto mt-16 max-w-3xl sm:mt-20">
         <InvoiceForm />
